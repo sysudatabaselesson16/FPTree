40c0c31aa68e05c7b0186422951671e8eb215313
Cynosure159 <15217310106@163.com>
2019-05-04 22:02:19 +0800

    系统说明书1.0

---
+++ Programming-FPTree/SystemManuals.md
@@ -0,0 +1,280 @@
+# FPTreeBD 系统说明书
+
+此文档是对于课程设计 **FPTreeBD** 的系统说明书，由于是根据基础模板上进行补全开发，系统说明书的很多细节会在逐步完成的过程中
+补全。
+
+## 1 引言
+
+### 1.1 编写目的
+
+此说明书是对本次课程设计的需求明确，以及对后续开发过程中的主要依据和详细说明。
+
+### 1.2 背景
+
+项目名称: FPTreeBD键值储存系统
+项目组成员: 陈扬、陈伟松、陆记、牛凌宇
+
+### 1.3 参考资料
+.
+
+## 2 系统介绍
+
+### 2.1 系统需求
+
+本次课程设计是实现一个简单的键值储存系统，其原理是基于针对NVM优化的数据结构FPTree。最终包装成一个调用库，并且对外可用的对数据实现4种基本操作:
+
+1. Insert 增加数据
+2. Remove 删除数据
+3. Update 更改数据
+4. Find 查询数据
+
+实现的是单线程的FPTree。
+
+### 2.2 系统结构
+
+系统基于数据结构FPTree，架构如下
+
+![FPTreeDB架构](./asset/FPTreeDB.png)
+
+### 2.3 项目目录结构
+
+```
+|__gtest: 为Google Test项目目录，不用管  
+|__include: 里包含所有用到的头文件  
+   |__fptree: fptree的头文件所在文件夹  
+      |__fptree.h: fptree地头文件  
+   |__utility: fptree所用工具的头文件所在文件夹  
+      |__utility.h: 指纹计算等工具函数所在头文件  
+      |__clhash.h: 指纹计算所用哈希函数头文件  
+      |__p_allocator.h: NVM内存分配器头文件  
+|__src: 为项目源码所在地，完成里面所有的实现  
+   |__bin: 可执行文件所在文件夹
+      |__main: main.cpp的可执行文件
+      |__lycsb: lycsb.cpp的可执行文件
+      |__ycsb: ycsb.cpp的可执行文件
+   |__fptree.cpp: fptree的源文件，项目核心文件
+   |__clhash.c: 指纹计算的哈希函数源文件  
+   |__p_allocator.cpp: NVM内存分配器源文件
+   |__lycsb.cpp: LevelDB的YCSB测试代码  
+   |__ycsb.cpp: FPTreeDB和LevelDB的YCSB对比测试代码  
+   |__makefile: src下项目的编译文件  
+|__workloads: 为YCSB测试负载文件，用于YCSB Benchmark测试  
+   |__数据量-rw-读比例-写比例-load.txt: YCSB测试数据库装载文件  
+   |__数据量-rw-读比例-写比例-run.txt: YCSB测试运行文件  
+|__test: 为Google Test用户测试代码所在，请完成编译并通过所有测试  
+   |__bin: 单元测试可执行文件所在文件夹
+      |__fptree_test: fptree_test.cpp的可执行文件
+      |__utility_test: utility_test.cpp的可执行文件
+   |__fptree_test.cpp: fptree相关测试  
+   |__utility_test.cpp: PAllocator等相关测试  
+   |__makefile: gtest单元测试的编译文件
+```
+### 2.4 项目检验标准
+
+通过使用 **YCSB测试** 和 **Google Test测试** 来检验此项目的完成情况
+
+## 3 系统实现
+
+### 3.1 FPTree
+
+这是整个键值存储系统的接口类
+
+#### bool FPTree::bulkLoading() {
+
+这是重建树的主要函数  
+先检查目标文件夹内有没有数据文件，遍历文件进行BulkLoading  
+没有数据文件则进行新树的生成
+
+![bulkLoading](./asset/bulkLoading.png)
+
+#### InnerNode* FPTree::getRoot()
+
+获得根节点
+
+#### void FPTree::changeRoot(InnerNode* newRoot)
+
+产生新的根节点
+
+#### void FPTree::printTree()
+
+打印树
+
+#### FPTree::insert(Key k, Value v)
+
+增加数据
+
+直接调用节点的函数
+
+#### bool FPTree::remove(Key k)
+
+删除数据
+
+直接调用节点的函数
+
+#### bool FPTree::update(Key k, Value v)
+
+更新数据
+
+直接调用节点的函数
+
+#### Value FPTree::find(Key k)
+
+查找数据
+
+直接调用节点的函数
+
+### 3.2 KeyNode
+
+这个数据结构由一个代表键值和节点的索引组成，用于节点分裂时，将新生成的节点索引返回给上层节点插入记录.  
+其用在插入操作和分裂操作中。 
+如下图  
+
+![insert](./asset/insert.png)
+
+### 3.3 InnerNode
+
+这是FPTree中间索引节点，其不存放键值对数据信息，结构如下：
+
+![InnerNode](./asset/InnerNode.png)
+
+#### int InnerNode::findIndex(const Key& k) 
+
+二分查找目标子节点
+
+#### void InnerNode::insertNonFull(const Key& k, Node* const& node)
+
+节点不满直接插入
+
+#### KeyNode* InnerNode::insert(const Key& k, const Value& v)
+
+键值对插入
+
+#### KeyNode* InnerNode::insertLeaf(const KeyNode& leaf) 
+
+插入叶子节点
+
+#### KeyNode* InnerNode::split()
+
+分裂满了的节点
+
+#### bool InnerNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool &ifDelete)、
+
+键值对删除
+
+#### void InnerNode::getBrother(const int& index, InnerNode* const& parent, InnerNode* &leftBro, InnerNode* &rightBro)
+
+获得中间节点的左右兄弟节点。如果左右兄弟都存在，统一处理右兄弟。
+
+#### void InnerNode::mergeParentLeft(InnerNode* const& parent, InnerNode* const& leftBro)
+
+与父亲节点以及左兄弟合并
+
+#### void InnerNode::mergeParentRight(InnerNode* const& parent, InnerNode* const& rightBro)
+
+与父亲节点以及右兄弟合并
+
+#### void InnerNode::redistributeLeft(const int& index, InnerNode* const& leftBro, InnerNode* const& parent)
+
+与左兄弟重分布
+
+#### void InnerNode::redistributeRight(const int& index, InnerNode* const& rightBro, InnerNode* const& parent)
+
+与右兄弟重分布
+
+#### void InnerNode::mergeLeft(InnerNode* const& leftBro, const Key& k)
+
+与左兄弟合并
+
+#### void InnerNode::mergeRight(InnerNode* const& rightBro, const Key& k)
+
+与右兄弟合并
+
+#### void InnerNode::removeChild(const int& keyIdx, const int& childIdx)
+
+删除儿子
+
+#### bool InnerNode::update(const Key& k, const Value& v)
+
+键值对修改
+
+#### Value InnerNode::find(const Key& k)
+
+键值对查询
+
+#### Node* InnerNode::getChild(const int& idx)
+
+获得儿子
+
+#### Key InnerNode::getKey(const int& idx)
+
+获得key
+
+#### void InnerNode::printNode()
+
+打印节点
+
+### 3.4 LeafNode
+
+这是整个FPTree存储数据的直接对象，所有键值对数据只存放于叶子节点中。  
+叶子节点也是与NVM交互的对象，只要是操作PAllocator映射NVM文件后的虚拟地址，通过文件映射的方式操作相应数据文件。  
+因为节点的操作对象是内存映射文件数据后的虚拟地址，所以关键是设置好NVM数据的指针。结构如下：  
+
+![LeafNode](./asset/LeafNode.png)
+
+#### KeyNode* LeafNode::insert(const Key& k, const Value& v)
+
+键值对插入
+
+#### void LeafNode::insertNonFull(const Key& k, const Value& v)
+
+节点不满直接插入
+
+#### KeyNode* LeafNode::split()
+
+分裂
+
+#### Key LeafNode::findSplitKey()
+
+找到中间值的key
+
+#### int LeafNode::getBit(const int& idx)
+
+得到bit
+
+#### Key LeafNode::getKey(const int& idx)
+
+得到key
+
+#### Value LeafNode::getValue(const int& idx)
+
+得到值
+
+#### PPointer LeafNode::getPPointer()
+
+得到指针
+
+#### bool LeafNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool &ifDelete)
+
+键值对删除
+
+#### bool LeafNode::update(const Key& k, const Value& v)
+
+键值对修改
+
+#### Value LeafNode::find(const Key& k)
+
+键值对查询
+
+#### int LeafNode::findFirstZero() 
+
+找到第一个空节点
+
+#### void LeafNode::persist()
+
+保存真个叶子
+
+### 3.5 PAllocator
+
+### 3.6 ycsb
+
+## 4 实现时间计划
--- Programming-FPTree/src/fptree.cpp
+++ Programming-FPTree/src/fptree.cpp
@@ -1,19 +1,22 @@
-#include"fptree/fptree.h"
+#include "fptree/fptree.h"
 
 using namespace std;
 
 // Initial the new InnerNode
-InnerNode::InnerNode(const int& d, FPTree* const& t, bool _isRoot) {
+InnerNode::InnerNode(const int& d, FPTree* const& t, bool _isRoot)
+{
     // TODO
 }
 
 // delete the InnerNode
-InnerNode::~InnerNode() {
+InnerNode::~InnerNode()
+{
     // TODO
 }
 
 // binary search the first key in the innernode larger than input key
-int InnerNode::findIndex(const Key& k) {
+int InnerNode::findIndex(const Key& k)
+{
     // TODO
     return 0;
 }
@@ -24,13 +27,15 @@ int InnerNode::findIndex(const Key& k) {
 // | key | node pointer |
 // ======================
 // WARNING: can not insert when it has no entry
-void InnerNode::insertNonFull(const Key& k, Node* const& node) {
+void InnerNode::insertNonFull(const Key& k, Node* const& node)
+{
     // TODO
 }
 
 // insert func
 // return value is not NULL if split, returning the new child and a key to insert
-KeyNode* InnerNode::insert(const Key& k, const Value& v) {
+KeyNode* InnerNode::insert(const Key& k, const Value& v)
+{
     KeyNode* newChild = NULL;
 
     // 1.insertion to the first leaf(only one leaf)
@@ -38,7 +43,7 @@ KeyNode* InnerNode::insert(const Key& k, const Value& v) {
         // TODO
         return newChild;
     }
-    
+
     // 2.recursive insertion
     // TODO
     return newChild;
@@ -47,14 +52,15 @@ KeyNode* InnerNode::insert(const Key& k, const Value& v) {
 // ensure that the leaves inserted are ordered
 // used by the bulkLoading func
 // inserted data: | minKey of leaf | LeafNode* |
-KeyNode* InnerNode::insertLeaf(const KeyNode& leaf) {
+KeyNode* InnerNode::insertLeaf(const KeyNode& leaf)
+{
     KeyNode* newChild = NULL;
     // first and second leaf insertion into the tree
     if (this->isRoot && this->nKeys == 0) {
         // TODO
         return newChild;
     }
-    
+
     // recursive insert
     // Tip: please judge whether this InnerNode is full
     // next level is not leaf, just insertLeaf
@@ -66,9 +72,10 @@ KeyNode* InnerNode::insertLeaf(const KeyNode& leaf) {
     return newChild;
 }
 
-KeyNode* InnerNode::split() {
+KeyNode* InnerNode::split()
+{
     KeyNode* newChild = new KeyNode();
-    // right half entries of old node to the new node, others to the old node. 
+    // right half entries of old node to the new node, others to the old node.
     // TODO
 
     return newChild;
@@ -77,78 +84,91 @@ KeyNode* InnerNode::split() {
 // remove the target entry
 // return TRUE if the children node is deleted after removement.
 // the InnerNode need to be redistributed or merged after deleting one of its children node.
-bool InnerNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool &ifDelete) {
+bool InnerNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool& ifDelete)
+{
     bool ifRemove = false;
     // only have one leaf
     // TODO
-    
+
     // recursive remove
     // TODO
     return ifRemove;
 }
 
 // If the leftBro and rightBro exist, the rightBro is prior to be used
-void InnerNode::getBrother(const int& index, InnerNode* const& parent, InnerNode* &leftBro, InnerNode* &rightBro) {
+void InnerNode::getBrother(const int& index, InnerNode* const& parent, InnerNode*& leftBro, InnerNode*& rightBro)
+{
     // TODO
 }
 
 // merge this node, its parent and left brother(parent is root)
-void InnerNode::mergeParentLeft(InnerNode* const& parent, InnerNode* const& leftBro) {
+void InnerNode::mergeParentLeft(InnerNode* const& parent, InnerNode* const& leftBro)
+{
     // TODO
 }
 
 // merge this node, its parent and right brother(parent is root)
-void InnerNode::mergeParentRight(InnerNode* const& parent, InnerNode* const& rightBro) {
+void InnerNode::mergeParentRight(InnerNode* const& parent, InnerNode* const& rightBro)
+{
     // TODO
 }
 
 // this node and its left brother redistribute
 // the left has more entries
-void InnerNode::redistributeLeft(const int& index, InnerNode* const& leftBro, InnerNode* const& parent) {
+void InnerNode::redistributeLeft(const int& index, InnerNode* const& leftBro, InnerNode* const& parent)
+{
     // TODO
 }
 
 // this node and its right brother redistribute
 // the right has more entries
-void InnerNode::redistributeRight(const int& index, InnerNode* const& rightBro, InnerNode* const& parent) {
+void InnerNode::redistributeRight(const int& index, InnerNode* const& rightBro, InnerNode* const& parent)
+{
     // TODO
 }
 
 // merge all entries to its left bro, delete this node after merging.
-void InnerNode::mergeLeft(InnerNode* const& leftBro, const Key& k) {
+void InnerNode::mergeLeft(InnerNode* const& leftBro, const Key& k)
+{
     // TODO
 }
 
 // merge all entries to its right bro, delete this node after merging.
-void InnerNode::mergeRight(InnerNode* const& rightBro, const Key& k) {
+void InnerNode::mergeRight(InnerNode* const& rightBro, const Key& k)
+{
     // TODO
 }
 
 // remove a children from the current node, used by remove func
-void InnerNode::removeChild(const int& keyIdx, const int& childIdx) {
+void InnerNode::removeChild(const int& keyIdx, const int& childIdx)
+{
     // TODO
 }
 
 // update the target entry, return true if the update succeed.
-bool InnerNode::update(const Key& k, const Value& v) {
+bool InnerNode::update(const Key& k, const Value& v)
+{
     // TODO
     return false;
 }
 
 // find the target value with the search key, return MAX_VALUE if it fails.
-Value InnerNode::find(const Key& k) {
+Value InnerNode::find(const Key& k)
+{
     // TODO
     return MAX_VALUE;
 }
 
 // get the children node of this InnerNode
-Node* InnerNode::getChild(const int& idx) {
+Node* InnerNode::getChild(const int& idx)
+{
     // TODO
     return NULL;
 }
 
 // get the key of this InnerNode
-Key InnerNode::getKey(const int& idx) {
+Key InnerNode::getKey(const int& idx)
+{
     if (idx < this->nKeys) {
         return this->keys[idx];
     } else {
@@ -157,54 +177,64 @@ Key InnerNode::getKey(const int& idx) {
 }
 
 // print the InnerNode
-void InnerNode::printNode() {
+void InnerNode::printNode()
+{
     cout << "||#|";
     for (int i = 0; i < this->nKeys; i++) {
         cout << " " << this->keys[i] << " |#|";
     }
-    cout << "|" << "    ";
+    cout << "|"
+         << "    ";
 }
 
 // print the LeafNode
-void LeafNode::printNode() {
+void LeafNode::printNode()
+{
     cout << "||";
     for (int i = 0; i < 2 * this->degree; i++) {
         if (this->getBit(i)) {
             cout << " " << this->kv[i].k << " : " << this->kv[i].v << " |";
         }
     }
-    cout << "|" << " ====>> ";
+    cout << "|"
+         << " ====>> ";
 }
 
 // new a empty leaf and set the valuable of the LeafNode
-LeafNode::LeafNode(FPTree* t) {
+LeafNode::LeafNode(FPTree* t)
+{
     // TODO
 }
 
 // reload the leaf with the specific Persistent Pointer
 // need to call the PAllocator
-LeafNode::LeafNode(PPointer p, FPTree* t) {
+LeafNode::LeafNode(PPointer p, FPTree* t)
+{
     // TODO
 }
 
-LeafNode::~LeafNode() {
+LeafNode::~LeafNode()
+{
     // TODO
 }
 
 // insert an entry into the leaf, need to split it if it is full
-KeyNode* LeafNode::insert(const Key& k, const Value& v) {
+KeyNode* LeafNode::insert(const Key& k, const Value& v)
+{
     KeyNode* newChild = NULL;
     // TODO
     return newChild;
 }
 
 // insert into the leaf node that is assumed not full
-void LeafNode::insertNonFull(const Key& k, const Value& v) {
+void LeafNode::insertNonFull(const Key& k, const Value& v)
+{
     // TODO
 }
 
 // split the leaf node
-KeyNode* LeafNode::split() {
+KeyNode* LeafNode::split()
+{
     KeyNode* newChild = new KeyNode();
     // TODO
     return newChild;
@@ -213,7 +243,8 @@ KeyNode* LeafNode::split() {
 // use to find a mediant key and delete entries less then middle
 // called by the split func to generate new leaf-node
 // qsort first then find
-Key LeafNode::findSplitKey() {
+Key LeafNode::findSplitKey()
+{
     Key midKey = 0;
     // TODO
     return midKey;
@@ -221,27 +252,32 @@ Key LeafNode::findSplitKey() {
 
 // get the targte bit in bitmap
 // TIPS: bit operation
-int LeafNode::getBit(const int& idx) {
+int LeafNode::getBit(const int& idx)
+{
     // TODO
     return 0;
 }
 
-Key LeafNode::getKey(const int& idx) {
+Key LeafNode::getKey(const int& idx)
+{
     return this->kv[idx].k;
 }
 
-Value LeafNode::getValue(const int& idx) {
+Value LeafNode::getValue(const int& idx)
+{
     return this->kv[idx].v;
 }
 
-PPointer LeafNode::getPPointer() {
+PPointer LeafNode::getPPointer()
+{
     return this->pPointer;
 }
 
 // remove an entry from the leaf
 // if it has no entry after removement return TRUE to indicate outer func to delete this leaf.
 // need to call PAllocator to set this leaf free and reuse it
-bool LeafNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool &ifDelete) {
+bool LeafNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool& ifDelete)
+{
     bool ifRemove = false;
     // TODO
     return ifRemove;
@@ -249,32 +285,37 @@ bool LeafNode::remove(const Key& k, const int& index, InnerNode* const& parent, 
 
 // update the target entry
 // return TRUE if the update succeed
-bool LeafNode::update(const Key& k, const Value& v) {
+bool LeafNode::update(const Key& k, const Value& v)
+{
     bool ifUpdate = false;
     // TODO
     return ifUpdate;
 }
 
 // if the entry can not be found, return the max Value
-Value LeafNode::find(const Key& k) {
+Value LeafNode::find(const Key& k)
+{
     // TODO
     return MAX_VALUE;
 }
 
 // find the first empty slot
-int LeafNode::findFirstZero() {
+int LeafNode::findFirstZero()
+{
     // TODO
     return -1;
 }
 
 // persist the entire leaf
 // use PMDK
-void LeafNode::persist() {
+void LeafNode::persist()
+{
     // TODO
 }
 
 // call by the ~FPTree(), delete the whole tree
-void FPTree::recursiveDelete(Node* n) {
+void FPTree::recursiveDelete(Node* n)
+{
     if (n->isLeaf) {
         delete n;
     } else {
@@ -285,34 +326,40 @@ void FPTree::recursiveDelete(Node* n) {
     }
 }
 
-FPTree::FPTree(uint64_t t_degree) {
+FPTree::FPTree(uint64_t t_degree)
+{
     FPTree* temp = this;
     this->root = new InnerNode(t_degree, temp, true);
     this->degree = t_degree;
     bulkLoading();
 }
 
-FPTree::~FPTree() {
+FPTree::~FPTree()
+{
     recursiveDelete(this->root);
 }
 
 // get the root node of the tree
-InnerNode* FPTree::getRoot() {
+InnerNode* FPTree::getRoot()
+{
     return this->root;
 }
 
 // change the root of the tree
-void FPTree::changeRoot(InnerNode* newRoot) {
+void FPTree::changeRoot(InnerNode* newRoot)
+{
     this->root = newRoot;
 }
 
-void FPTree::insert(Key k, Value v) {
+void FPTree::insert(Key k, Value v)
+{
     if (root != NULL) {
         root->insert(k, v);
     }
 }
 
-bool FPTree::remove(Key k) {
+bool FPTree::remove(Key k)
+{
     if (root != NULL) {
         bool ifDelete = false;
         InnerNode* temp = NULL;
@@ -321,14 +368,16 @@ bool FPTree::remove(Key k) {
     return false;
 }
 
-bool FPTree::update(Key k, Value v) {
+bool FPTree::update(Key k, Value v)
+{
     if (root != NULL) {
         return root->update(k, v);
     }
     return false;
 }
 
-Value FPTree::find(Key k) {
+Value FPTree::find(Key k)
+{
     if (root != NULL) {
         return root->find(k);
     }
@@ -336,7 +385,8 @@ Value FPTree::find(Key k) {
 
 // call the InnerNode and LeafNode print func to print the whole tree
 // TIPS: use Queue
-void FPTree::printTree() {
+void FPTree::printTree()
+{
     // TODO
 }
 
@@ -344,7 +394,8 @@ void FPTree::printTree() {
 // need to traverse leaves chain
 // if no tree is reloaded, return FALSE
 // need to call the PALlocator
-bool FPTree::bulkLoading() {
+bool FPTree::bulkLoading()
+{
     // TODO
     return false;
 }
